b8e7e832d0d5161da09141d94b9bd1b87f54bbd8
Manjot Singh <55556349+Manzot@users.noreply.github.com>
2020-05-14 14:38:11 -0400

    Added Website Template

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  console.log('footer js initilized.');
+
+	/*------------------
+		Navigation
+	--------------------*/
+  $('.nav-switch').on('click', function (event) {
+    $('.main-menu').slideToggle(400);
+    event.preventDefault();
+  });
+
+
+	/*------------------
+		Accordions
+	--------------------*/
+  $('.panel-link').on('click', function (e) {
+    $('.panel-link').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+      $this.addClass('active');
+    }
+    e.preventDefault();
+  });
+
+
+  //back to top
+  backToTop();
+  function backToTop() {
+    $(".back-to-top").css("display", "none");
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 0) {
+        $(".back-to-top").fadeIn(300);
+      } else {
+        $(".back-to-top").fadeOut(300);
+      }
+    });
+    $(".back-to-top").click(function () {
+      console.log('boom');
+      $("html, body").animate({
+        scrollTop: 0
+      }, 500);
+    });
+  };
+
+  //Smooth scrolling with links
+  $('.main-menu a[href*=\\#]').on('click', function (event) {
+    event.preventDefault();
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
+    $('.navbar-toggler').trigger('click'); // to close navbar
+  });
+
+});
+
